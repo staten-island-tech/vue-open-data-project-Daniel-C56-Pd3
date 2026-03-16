19734d7bede624874dcd44ddb637817b0572757b
Daniel <danield494@nycstudents.net>
2026-03-16 14:01:50 -0400

    1

---
+++ Data/src/vues/home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="health-container">
+        <HealthCard
+            v-for="()"
+        />
+    </div>
+</template>
+
+<script setup>
+import{ref, onMounted} from 'vue'
+import 
+https://data.cityofnewyork.us/resource/jb7j-dtam.json?$limit=50000
+</script>
+
+<style>
+
+</style>
