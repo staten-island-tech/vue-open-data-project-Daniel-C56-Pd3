129e49317e23c688dda71d78508b980dca5b35cd
Daniel <danield494@nycstudents.net>
2026-03-17 14:02:01 -0400

    1

--- Data/src/vues/home.vue
+++ Data/src/vues/home.vue
@@ -1,17 +1,28 @@
 <template>
-    <div class="health-container">
-        <HealthCard
-            v-for="()"
-        />
-    </div>
+  <div>
+    <p v-for="healthData in healthData">{{ healthData.leading_cause }}</p>
+  </div>
 </template>
 
 <script setup>
-import{ref, onMounted} from 'vue'
-import 
-https://data.cityofnewyork.us/resource/jb7j-dtam.json?$limit=50000
+import { ref, onMounted } from 'vue'
+const healthData = ref([])
+
+async function getData() {
+  try {
+    const response = await fetch(
+      'https://data.cityofnewyork.us/resource/jb7j-dtam.json?$limit=50000',
+    )
+    const data = await response.json()
+    healthData.value = data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+onMounted(() => {
+  getData()
+})
 </script>
 
-<style>
-
-</style>
+<style scoped></style>
