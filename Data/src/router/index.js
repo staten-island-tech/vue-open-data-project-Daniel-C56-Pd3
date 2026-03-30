fe04d36616c4f4abe236b6bb1689d64156a62d27
Daniel <danield494@nycstudents.net>
2026-03-29 22:13:43 -0400

    1

--- Data/src/router/index.js
+++ Data/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/home.vue'
-import HealthData from '@/views/healthdata.vue' 
+import HomeView from '@/vues/home.vue'
+import HealthData from '@/vues/healthdata.vue' 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
